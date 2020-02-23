c6640adfd9ed947ce16419349070934c18f972da
zhangwenzhuo <zhangwz@polysoft.com.cn>
2020-02-24 00:01:07 +0800

    趋势这一模块的modal的自定义

--- js/page/TrendingPage.js
+++ js/page/TrendingPage.js
@@ -1,6 +1,6 @@
 
 import React, {Component} from 'react';
-import { StyleSheet, Text, View,Button,FlatList,RefreshControl,ActivityIndicator} from 'react-native';
+import { StyleSheet, Text, View,Button,FlatList,RefreshControl,ActivityIndicator,TouchableOpacity,DeviceEventEmitter} from 'react-native';
 import {createMaterialTopTabNavigator} from 'react-navigation-tabs'//createTabNavigator
 import {createAppContainer} from 'react-navigation'
 import NavigationUtil from '../navigator/NavigationUtil';
@@ -13,18 +13,24 @@ const URL =`https://github.com/trending/`;
 const QUERY_STR = `&sort=stars`;
 const THEME_COLOR='red';
 import NavigationBar from '../common/NavigationBar'
+import TrendingDialog,{TimeSpans}  from '../common/TrendingDialog'
+import  MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+const EVENT_TYPE_TIME_SPAN_CHANGE ='EVENT_TYPE_TIME_SPAN_CHANGE';
 
 export default class TrendingPage extends React.Component {
     constructor(props){
         super(props)
-        this.tabNames=['c','c#','php','javascript'] //视频里的all  这个模块 里面没有数据
+        this.tabNames=['c','c#','php','javascript'];//视频里的all  这个模块 里面没有数据
+        this.state={
+            timeSpan:TimeSpans[2],
+        }
     }
     //动态生成tab
     _genTabs(){
         const tans={};
         this.tabNames.forEach((item,index)=>{
             tans[`tab${index}`] ={//`tab&{index 设置唯一的路由名字  //在这块出了问题  const tans={};定义的tans   tans[`tab${index}`] 这一块写成了 tabs[`tab${index}`] 报错信息找不到tabs 低级错误
-                screen:props=><TrendingTabPage {...props} tabLabel={item}/>,//这个是动态设置属性 我们一般是直接跳转某个界面
+                screen:props=><TrendingTabPage {...props} timeSpan={this.state.timeSpan} tabLabel={item}/>,//这个是动态设置属性 我们一般是直接跳转某个界面
                 navigationOptions:{
                     title:item,
                 }
@@ -32,40 +38,86 @@ export default class TrendingPage extends React.Component {
         });
         return tans
     }
+    renderTitleView(){
+        return <View>
+            <TouchableOpacity
+            underlayColor='transparent'
+            onPress={()=>this.dialog.show()}>
+                <View style={{flexDirection:'row',alignItems: 'center' }}>
+                    <Text style={{fontSize:18,color:'#FFFFFF',fontWeight: '400'}}>
+                        趋势 {this.state.timeSpan.showText}
+                    </Text>
+                    <MaterialIcons
+                    name={'arrow-drop-down'}
+                    size={22}
+                    style={{color:'white'}}
+                    />
+                </View>
+            </TouchableOpacity>
+        </View>
+    }
+
+    onSelectTimeSpan(tab){
+        debugger
+        this.dialog.dismiss();
+        this.setState({
+            //这里出现了问题 setstate  后面给写上=号 导致选择上的状态更新不了  这个问题 卡了不短时间 一直没注意到 知道是这里的问题但是没有在意 太失误了 写上=号就赋不上值了
+            timeSpan:tab,
+            // console.log('到底获取到值没有',timeSpan)
+        })
+        DeviceEventEmitter.emit(EVENT_TYPE_TIME_SPAN_CHANGE,tab)
+
+    }
+    renderTrendingDialog(){
+        return <TrendingDialog
+        ref={dialog=>this.dialog=dialog }
+        onSelect={tab=>this.onSelectTimeSpan(tab)}
+        />
+    }
+
+    _tabNav(){
+        if(!this.tabNav){
+            this.tabNav=createAppContainer(
+                createMaterialTopTabNavigator(
+                    this._genTabs(),
+                    //设置样式
+                    {
+                        tabBarOptions:{
+                            tabStyle:styles.tabStyle,
+                            upperCaseLabel:false,
+                            scrollEnabled:true,
+                            style:{
+                                backgroundColor:'#234571'
+                            },
+                            indicatorStyle:styles.indicatorStyle,
+                            labelStyle:styles.labelStyle,
+                        }
+                    }
+
+                )
+            );
+        }
+        return this.tabNav;
+
+    }
     render() {
         let statusBar = {
             backgroundColor:THEME_COLOR,
             barStyle:'light-content'
         }
         let navigationBar = <NavigationBar
-            title={'趋势'}
+            // title={'趋势'}
+            titleView={this.renderTitleView()}
             statusBar={statusBar}
             style={{backgroundColor:THEME_COLOR}}
 
         />
-        const TabNavigator=createAppContainer(
-            createMaterialTopTabNavigator(
-                this._genTabs(),
-                //设置样式
-                {
-                    tabBarOptions:{
-                        tabStyle:styles.tabStyle,
-                        upperCaseLabel:false,
-                        scrollEnabled:true,
-                        style:{
-                            backgroundColor:'#234571'
-                        },
-                        indicatorStyle:styles.indicatorStyle,
-                        labelStyle:styles.labelStyle,
-                    }
-                }
-
-            )
-        );
+        const  TabNavigator = this._tabNav();
         return (
             <View style={styles.container}>
                 {navigationBar}
                 <TabNavigator/>
+                {this.renderTrendingDialog()}
             </View>
         );
     }
@@ -74,11 +126,22 @@ const  pageSize = 10;//设置常量 防止修改
 class TrendingTab extends Component{
     constructor(props){
         super(props)
-        const {tabLabel}=this.props;
+        const {tabLabel,timeSpan}=this.props;
         this.storeName=tabLabel;
+        this.timeSpan=timeSpan;
     }
     componentDidMount(){
         this.loadData();
+        this.timeSpanChangeListener = DeviceEventEmitter.addListener(EVENT_TYPE_TIME_SPAN_CHANGE,(timeSpan)=>{
+            this.timeSpan=timeSpan;
+            this.loadData();
+        })
+    }
+
+    componentWillUnmount(){
+        if(this.timeSpanChangeListener){
+            this.timeSpanChangeListener.remove();
+        }
     }
     loadData(loadMore){
         const  {onRefreshTrending,onLoadMoreTrending}=this.props;
@@ -107,7 +170,7 @@ class TrendingTab extends Component{
         return store;
     }
     getFetchUrl(key){
-        return URL+key+`?since=daily`;
+        return URL+key+`?`+this.timeSpan.searchText;//`?since=daily`
     }
     renderItem(data){
         const item=data.item;
